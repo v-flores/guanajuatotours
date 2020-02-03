019070d0999260093ad742feaa746777e22e1515
v-flores <44541135+v-flores@users.noreply.github.com>
2020-02-02 18:39:52 -0600

    tourData creation

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -19,14 +19,19 @@ export default class Tour extends Component {
       <article className="tour">
         <div className="img-container">
           <img src={img} alt="guanajuato tour" />
-          <span className="close-btn" onClick={()=>removeTour(id)}>
+          <span className="close-btn" onClick={() => removeTour(id)}>
             <i className="fas fa-window-close"></i>
           </span>
         </div>
         <div className="tour-info">
-          <h3>{city}</h3>
+          <h2>{name}</h2>
+          <i className="fas fa-money-bill"></i>
           <h3>${price}</h3>
-          <h3>{name}</h3>
+          <br></br>
+          <i className="fas fa-map-marker"></i>
+          <h3>{city}</h3>
+          <br></br>
+          <b>More Info:</b>
           {this.state.showInfo && <p>{info}</p>}
           <span onClick={this.handleInfo}>
             <i className="fas fa-caret-square-down"></i>
--- src/tourData.js
+++ src/tourData.js
@@ -1,38 +1,56 @@
 export const tourData = [
   {
     id: 1,
-    city: "city1",
-    img: "./img/newyork.jpeg",
-    name: "name",
-    price: 45,
+    city: "San Miguel de Allende",
+    img: "./img/sanmiguel.jpg",
+    name: "San Miguel Walking Tour",
+    price: 55,
     info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      "Walking through the historic district of San Miguel de Allende is a great way to discover this town’s beautiful hidden enclaves and must-see attractions alike. This private San Miguel walking tour introduces you to the city’s charms with unparalleled personal attention."
   },
   {
     id: 2,
-    city: "city2",
-    img: "./img/paris.jpeg",
-    name: "name",
-    price: 34,
+    city: "San Miguel de Allende",
+    img: "./img/Atotonilco.jpg",
+    name: "Dolores Hidalgo & Sanctuary of Atotonilco",
+    price: 160,
     info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      "Visit the beautiful town of Dolores Hidalgo, cradle of Mexico’s Independence movement. See it’s magnificent church, it’s museums, local crafts and taste their delicious hand made ice cream flavors. After that, we’ll see the incredible paintings at the Atotonilco Sanctuary, a UNESCO World Heritage site"
   },
   {
     id: 3,
-    city: "city3",
-    img: "./img/london.jpeg",
-    name: "name",
-    price: 24,
+    city: "Guanajuato",
+    img: "./img/diegorivera.jpg",
+    name: "Guanajuato Diego Riviera and Frida Kahlo Tour",
+    price: 34,
     info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      "Frida Kahlo and Diego Rivera helped to define Mexican modernism while engaged in a tumultuous romance. On this 2-hour tour we’ll get a glimpse into the life of Mexican artists.Our visit will take us to the Diego Riviera Museum, his former abode and where many of his works were created. We will get a sense of how he once lived and worked. We’ll engage directly with his history as we surround ourselves by his artwork, clothing, photos"
   },
   {
     id: 4,
-    city: "city4",
-    img: "./img/tokyo.jpeg",
-    name: "name",
-    price: 45,
+    city: "Penjamo",
+    img: "./img/corralejo.jpg",
+    name: "Tequilera Corralejo",
+    price: 30,
     info:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      "Hacienda Corralejo is a great place to meet , to go with family, friends and enjoy a unique place . As you approach , you begin to feel a special thrill , you get to see his tall, slender palm trees around, and welcoming visitors. Since you have accessed the Treasury, visitors are welcomed by a group of guides that lead to know each of their spaces and especially the process that is Tequila Corralejo, the best Tequila of Mexico for the World."
+  },
+  {
+    id: 4,
+    city: "Guanajuato",
+    img: "./img/callejoneadas.jpg",
+    name: "Guanajuato Nightlife and Callejoneadas Tour",
+    price: 34,
+    info:
+      "Join a traditional Callejoneadas and walk along the beautiful streets of Guanajuato while you listen to the musical performances of the Estudiantinas who accompany you. Singing both original and traditional Mexican songs, these young performers are dress in delightfully old fashioned clothing and play various instruments, including percussion and string instruments along with the accordion."
+  },
+  {
+    id: 4,
+    city: "San Miguel de Allende",
+    img: "./img/tasteofsm.jpg",
+    name: "Taste of San Miguel Food Tour",
+    price: 52,
+    info:
+      "Discover San Miguel's most colorful flavors during a small-group walking tour led by a passionate food expert. Enjoy a variety of traditional Mexican culinary delights while supporting and interacting with the local food artisans.This food-tasting and cultural walking tour of San Miguel de Allende lets you admire Mexican culture from the perspective of a local, through delicious cuisine, rich history, and fascinating architecture. Suitable for all age groups, the tour whisks you away on a culinary journey from beginning to end."
   }
 ];
